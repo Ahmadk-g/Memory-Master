9b8c748300c6c4f05844b4f9e9a32f719d7a697b
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-03-11 13:34:36 +0100

    add a function to create and display cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,28 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    const cardValues = ["A", "B", "C", "D", "E", "F", "A", "B", "C", "D", "F"];
+    const cardValues = ["A", "B", "C", "D", "E", "F", "A", "B", "C", "D", "E", "F"];
     
     const cardContainer = this.getElementById("card-container") ;
 
     // Shuffle the card values
+    shuffleCards(cardValues);
+    
+    // Create and display the cards
+    cardValues.forEach((value, index) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('data-index', index);
+        card.textContent = value;
+        cardContainer.appendChild(card);
+      });
+
+});
+
+
+function shuffleCards(cardValues) {
     cardValues.sort(() => Math.random() - 0.5);
+}
 
 
-    for (let i of cardValues){
-        
-    }
-
-})
+    
